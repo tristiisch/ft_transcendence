3278d8d50fd1b244cf7b22b9c3cf031c698507f1
nlaronch <nlaronch@student.42Lyon.fr>
2022-07-16 14:32:26 +0200

    modif color css

--- front-end/src/views/Lobby.vue
+++ front-end/src/views/Lobby.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex justify-center items-center w-full h-full">
-        <div class="flex-col justify-center min-w-full xl:min-w-0 xl:w-4/6 min-h-[448px] md:min-h-[568px] bg-white bg-opacity-40 rounded-t-lg">
-           <div class="flex flex-col justify-around bg-lime-400 rounded-t-lg  h-44 md:h-32">
+    <div class="flex justify-center items-around w-full pt-32">
+        <div class="flex-col justify-center min-w-full xl:min-w-0 xl:w-4/6 min-h-[448px] md:min-h-[568px] rounded-t-lg  bg-slate-900">
+           <div class="flex flex-col justify-around bg-slate-900 rounded-t-lg  h-44 md:h-32">
                 <div class="flex justify-between items-center">
                     <div class="flex flex-col items-center md:flex-row">
                         <img src="@/assets/trophy.png" class="h-20 ml-4 p-4" />
@@ -11,7 +11,7 @@
                         <base-button class="mr-8">Action</base-button>
                     </div>
                 </div>
-                <div class="flex justify-center items-center bg-lime-400 text-xl">
+                <div class="flex justify-center items-centertext-xl">
                     <div class="text-center flex-1">
                         <sort-button>
                             Player
@@ -30,7 +30,7 @@
                 </div>
             </div>
             <div class="overflow-y-auto max-h-[316px] md:max-h-[444px]">
-                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden">
+                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden bg-gradient-to-r from-red-400 to-blue-500 hover:from-green-500 hover:to-lime-200 ">
                     <img class="absolute -left-6 -top-3 h-24 w-24 md:-left-8 md:-top-4 md:w-36 md:h-36 rounded-full object-cover" src="@/assets/obama.jpeg" alt="Rounded avatar">
                     <div class="flex items-center pl-20 md:pl-32">Robert</div>
                     <div class="flex justify-center items-center">
@@ -39,51 +39,54 @@
                     </div>
                     <div class="flex justify-center items-center"> 1 </div>
                 </div>
-                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden">
+                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden bg-gradient-to-r from-red-400 to-blue-500 hover:from-green-500 hover:to-lime-200 ">
                     <img class="absolute -left-6 -top-3 h-24 w-24 md:-left-8 md:-top-4 md:w-36 md:h-36 rounded-full object-cover" src="@/assets/obama.jpeg" alt="Rounded avatar">
-                    <div class="pl-20 md:pl-32 py-4">Robert</div>
-                    <div class="flex justify-center items-center py-4 pl-6">
+                    <div class="flex items-center pl-20 md:pl-32">Robert</div>
+                    <div class="flex justify-center items-center">
                             <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> 
                             Online
                     </div>
-                    <div class="text-center py-4"> 1 </div>
+                    <div class="flex justify-center items-center"> 1 </div>
                 </div>
-                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden">
+                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden bg-gradient-to-r from-red-400 to-blue-500 hover:from-green-500 hover:to-lime-200 ">
                     <img class="absolute -left-6 -top-3 h-24 w-24 md:-left-8 md:-top-4 md:w-36 md:h-36 rounded-full object-cover" src="@/assets/obama.jpeg" alt="Rounded avatar">
-                    <div class="pl-20 md:pl-32 py-4">Robert</div>
-                    <div class="flex justify-center items-center py-4 pl-6">
+                    <div class="flex items-center pl-20 md:pl-32">Robert</div>
+                    <div class="flex justify-center items-center">
                             <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> 
                             Online
                     </div>
-                    <div class="text-center py-4"> 1 </div>
+                    <div class="flex justify-center items-center"> 1 </div>
                 </div>
-                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden">
+                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden bg-gradient-to-r from-red-400 to-blue-500 hover:from-green-500 hover:to-lime-200 ">
                     <img class="absolute -left-6 -top-3 h-24 w-24 md:-left-8 md:-top-4 md:w-36 md:h-36 rounded-full object-cover" src="@/assets/obama.jpeg" alt="Rounded avatar">
-                    <div class="pl-20 md:pl-32 py-4">Robert</div>
-                    <div class="flex justify-center items-center py-4 pl-6">
+                    <div class="flex items-center pl-20 md:pl-32">Robert</div>
+                    <div class="flex justify-center items-center">
                             <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> 
                             Online
                     </div>
-                    <div class="text-center py-4"> 1 </div>
+                    <div class="flex justify-center items-center"> 1 </div>
                 </div>
-                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden">
+                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden bg-gradient-to-r from-red-400 to-blue-500 hover:from-green-500 hover:to-lime-200 ">
                     <img class="absolute -left-6 -top-3 h-24 w-24 md:-left-8 md:-top-4 md:w-36 md:h-36 rounded-full object-cover" src="@/assets/obama.jpeg" alt="Rounded avatar">
-                    <div class="pl-20 md:pl-32 py-4">Robert</div>
-                    <div class="flex justify-center items-center py-4 pl-6">
+                    <div class="flex items-center pl-20 md:pl-32">Robert</div>
+                    <div class="flex justify-center items-center">
                             <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> 
                             Online
                     </div>
-                    <div class="text-center py-4"> 1 </div>
+                    <div class="flex justify-center items-center"> 1 </div>
                 </div>
-                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden">
+                <div class="relative grid grid-flow-col auto-cols-fr place-content-center h-16 md:h-24 myshadow2 m-3 overflow-hidden bg-gradient-to-r from-red-400 to-blue-500 hover:from-green-500 hover:to-lime-200 ">
                     <img class="absolute -left-6 -top-3 h-24 w-24 md:-left-8 md:-top-4 md:w-36 md:h-36 rounded-full object-cover" src="@/assets/obama.jpeg" alt="Rounded avatar">
-                    <div class="pl-20 md:pl-32 py-4">Robert</div>
-                    <div class="flex justify-center items-center py-4 pl-6">
+                    <div class="flex items-center pl-20 md:pl-32">Robert</div>
+                    <div class="flex justify-center items-center">
                             <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> 
                             Online
                     </div>
-                    <div class="text-center py-4"> 1 </div>
+                    <div class="flex justify-center items-center"> 1 </div>
                 </div>
+               
+             
+
             </div>
         </div>
   </div>
@@ -113,7 +116,7 @@
 }
 
 .myshadow2 {
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.8);
 } 
 
 </style>
