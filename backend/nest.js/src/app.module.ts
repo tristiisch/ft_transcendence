724c09ac936538549301e8b0b33bd1af55909d55
tglory <tglory@student.42Lyon.fr>
2022-07-25 15:24:12 +0200

    format: remove test frontend

--- backend/nest.js/src/app.module.ts
+++ backend/nest.js/src/app.module.ts
@@ -1,23 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ServeStaticModule} from '@nestjs/serve-static';
-import { join } from 'path';
 import { UsersModule } from './users/users.module';
 
 @Module({
 	imports: [
-
-		/**
-		 * Controller for front (we won't used it)
-		 */
-		/*ServeStaticModule.forRoot({
-			rootPath: join(__dirname, '..', '../frontend/dist'),
-		}),*/
-
 		UsersModule,
 	],
- controllers: [AppController],
- providers: [AppService],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
