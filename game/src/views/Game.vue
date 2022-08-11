d0e07260ba768cbf4866c54c9bb2f78860c84b0c
Neben <neben@macben.local>
2022-08-11 22:44:56 +0200

    p1 blocker (responsivity and collisions done)

--- game/src/views/Game.vue
+++ game/src/views/Game.vue
@@ -9,14 +9,17 @@ const ball_size_quotient = 100 // the least, the bigger
 const ball_xpos_quotient = 2 // 2 being the center | min:1 max:inf
 const ball_ypos_quotient = 2
 
-const rect_width_quotient = 50
-const rect_height_quotient = 5
+const blocker_width_quotient = 50
+const blocker_height_quotient = 5
+const blocker_movements_delta = 30
+const blocker_xpos_quotient = 10
 
 onMounted(() => {
 	function computeStageHeight(): number { return window.innerHeight * (75 / 100) }
 	function computeBallSize(): number { return stage.width() / ball_size_quotient }
-	function computeRectWidth(): number { return stage.width() / rect_width_quotient }
-	function computeRectHeight(): number { return stage.height() / rect_height_quotient }
+	function computeBallSpeed(): number { return stage.width() / ball_speed_quotient }
+	function computeBlockerWidth(): number { return stage.width() / blocker_width_quotient }
+	function computeBlockerHeight(): number { return stage.height() / blocker_height_quotient }
 
 	var stage_height = computeStageHeight()
 	var stage_width = stage_height * stage_ratio
@@ -38,27 +41,64 @@ onMounted(() => {
 		fill: 'red'
 	})
 
-	var rect_p1 = new Konva.Rect({
-		x: stage.width() / 10,
-		y: stage.height() / 2.5,
-		width: computeRectWidth(),
-		height: computeRectHeight(),
+	var blockers_width = computeBlockerWidth()
+	var blockers_height = computeBlockerHeight()
+	var p1_blocker: Konva.Rect = new Konva.Rect({
+		width: blockers_width,
+		height: blockers_height,
+		x: stage.width() / blocker_xpos_quotient,
+		y: stage.height() / 2 - blockers_height / 2,
+		fill: 'purple'
+	})
+	var p2_blocker = new Konva.Rect({
+		width: blockers_width,
+		height: blockers_height,
+		x: stage.width() - stage.width() / blocker_xpos_quotient,
+		y: stage.height() / 2 - blockers_height / 2,
 		fill: 'purple'
 	})
 
 	layer.add(ball)
-	layer.add(rect_p1)
+	layer.add(p1_blocker)
+	layer.add(p2_blocker)
 
-	var ball_speed = stage.width() / ball_speed_quotient
+	var ball_speed = computeBallSpeed()
 	var dx = ball_speed
 	var dy = ball_speed
+	function check_stage_limits_collisions() {
+		if (ball.x() + dx < ball_radius || ball.x() + dx > stage_width - ball_radius) { dx = -dx; }
+		if (ball.y() + dy < ball_radius || ball.y() + dy > stage_height - ball_radius) { dy = -dy; }
+	}
+	function check_blockerp1_collisions() {
+		if (haveIntersection())
+			dx = -dx;
+	}
+	function haveIntersection() {
+		return !(
+			ball.x() + dx > p1_blocker.x() + p1_blocker.width() ||
+			ball.x() + dx < p1_blocker.x() ||
+			ball.y() + dy > p1_blocker.y() + p1_blocker.height() ||
+			ball.y() + dy < p1_blocker.y()
+		)
+	}
+	document.addEventListener("keydown", function(e) {
+		console.log(e.key)
+		if (e.key == 'ArrowDown') {
+			if (p1_blocker.y() + p1_blocker.height() + blocker_movements_delta <= stage.height())
+				p1_blocker.y(p1_blocker.y() + blocker_movements_delta)
+		}
+		else if (e.key == 'ArrowUp') {
+			if (p1_blocker.y() - blocker_movements_delta >= 0)
+				p1_blocker.y(p1_blocker.y() - blocker_movements_delta)
+		}
+	})
+
 	var ball_animation = new Konva.Animation(function(frame) {
-		if (ball.x() + dx > stage_width - ball_radius || ball.x() + dx < ball_radius) { dx = -dx; }
-		if (ball.y() + dy > stage_height - ball_radius || ball.y() + dy < ball_radius) { dy = -dy; }
+		check_stage_limits_collisions()
+		check_blockerp1_collisions()
 		ball.x(ball.x() + dx);
 		ball.y(ball.y() + dy);
-	}, layer);
-
+	}, layer)
 	ball_animation.start();
 
 	//--------------------------------------------------
@@ -70,11 +110,22 @@ onMounted(() => {
 		var ratio = stage.height() / stage_height
 		ball.x(ball.x() * ratio)
 		ball.y(ball.y() * ratio)
-		ball_speed = stage.width() / ball_speed_quotient
+		ball_speed = computeBallSpeed()
 		dx = dx > 0 ? ball_speed : -ball_speed
 		dy = dy > 0 ? ball_speed : -ball_speed
 		ball_radius = computeBallSize()
 		ball.radius(ball_radius)
+
+		blockers_width = computeBlockerWidth()
+		blockers_height = computeBlockerHeight()
+		p1_blocker.width(blockers_width)
+		p1_blocker.height(blockers_height)
+		p1_blocker.x(p1_blocker.x() * ratio)
+		p1_blocker.y(p1_blocker.y() * ratio)
+		p2_blocker.width(blockers_width)
+		p2_blocker.height(blockers_height)
+		p2_blocker.x(p2_blocker.x() * ratio)
+		p2_blocker.y(p2_blocker.y() * ratio)
 
 		stage_height = stage.height()
 		stage_width = stage.width()
