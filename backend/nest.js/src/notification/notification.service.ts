c7eb21ea2efad29e9e083bbccc4c32ad7092f775
Tristiisch <tristiisch@outlook.fr>
2022-08-24 21:30:33 +0200

    up: add notification action in controller

--- backend/nest.js/src/notification/notification.service.ts
+++ backend/nest.js/src/notification/notification.service.ts
@@ -1,10 +1,12 @@
 /** @prettier */
-import { forwardRef, Inject, Injectable, NotImplementedException, PreconditionFailedException } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, NotFoundException, NotImplementedException, PreconditionFailedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FriendsService } from 'src/friends/friends.service';
 import { User } from 'src/users/entity/user.entity';
 import { UsersService } from 'src/users/users.service';
-import { InsertResult, Repository } from 'typeorm';
+import { InsertResult, Repository, SelectQueryBuilder } from 'typeorm';
+import { NotificationAction } from './entity/notification-action.entity';
+import { NotificationFront } from './entity/notification-front.entity';
 import { Notification, NotificationType } from './entity/notification.entity';
 
 @Injectable()
@@ -48,8 +50,33 @@ export class NotificationService {
 		return await this.notifsRepository.insert(notif);
 	}
 
-	// TODO sort notifications
-	public async findMany(userId: number): Promise<Notification[]> {
-		return await this.notifsRepository.findBy({ user_id: userId });
+	public async findMany(userId: number): Promise<NotificationFront[]> {
+		const sqlStatement: SelectQueryBuilder<Notification> = this.notifsRepository.createQueryBuilder('notif');
+
+		sqlStatement.orderBy('notif.id', 'DESC', 'NULLS LAST');
+		return await sqlStatement.getMany().then(async (notifs: Notification[]) => {
+			const allNotifsFront: NotificationFront[] = new Array();
+			for (let notif of notifs) {
+				const notifFront: NotificationFront = new NotificationFront();
+				const target: User = await this.userService.findOne(notif.from_user_id);
+				notifFront.id = notif.id;
+				notifFront.user_id = notif.user_id;
+				if (notif.type === NotificationType.FRIEND_REQUEST) {
+					notifFront.message = `${notif.from_user_id} sent you a friend request.`;
+				} else if (notif.type === NotificationType.MATCH_REQUEST) {
+					notifFront.message = `${notif.from_user_id} wants to play Pong with you.`;
+				}
+				allNotifsFront.push(notifFront);
+			}
+			return allNotifsFront;
+		}, this.userService.lambdaDatabaseUnvailable);
+	}
+
+	async action(user: User, notifAction: NotificationAction): Promise<boolean> {
+		const notif: Notification = await this.notifsRepository.findOneBy({ id: notifAction.id });
+		if (!notif)
+			throw new NotFoundException(`Unable to find notification n°${notifAction.id}.`);
+		const func = this.notifActions.get(notif.type);
+		return func.call(null, notif, user, notifAction.accept);
 	}
 }
