91af384913c949d2d47b98fc4a63df3971e65f98
Tristiisch <tristiisch@outlook.fr>
2022-08-30 18:03:58 +0200

    up: chat controller

--- backend/nest.js/src/chat/chat.controller.ts
+++ backend/nest.js/src/chat/chat.controller.ts
@@ -9,108 +9,110 @@ import { DiscussionFront } from './entity/discussion.entity';
 
 @Controller('chat')
 export class ChatController {
-    
-	@Inject(ChatService)
-	private readonly chatService: ChatService;
+	
+	constructor(private readonly chatService: ChatService) {}
+
 	@Inject(UsersService)
 	private readonly usersService: UsersService;
 
 	@UseGuards(JwtAuthGuard)
-    @Get('channels')
-    getChannels() {
-        return this.chatService.findAllChannels();
-    }
+	@Get('channels')
+	getChannels() {
+		return this.chatService.findAllChannels();
+	}
 
 	@UseGuards(JwtAuthGuard)
-    @Get('user-discussions')
-    getUserPrivateMessage(@Req() req) {
+	@Get('user-discussions')
+	getUserPrivateMessage(@Req() req) {
 		const user: User = req.user;
-        throw new NotImplementedException('This feature is under developpement.');
-    }
+		throw new NotImplementedException('This feature is under developpement.');
+	}
 
 	@UseGuards(JwtAuthGuard)
-    @Get('user-channels')
-    getUserChannels(@Req() req) {
+	@Get('user-channels')
+	getUserChannels(@Req() req) {
 		const user: User = req.user;
-        throw new NotImplementedException('This feature is under developpement.');
-    }
+		throw new NotImplementedException('This feature is under developpement.');
+	}
 
 	@UseGuards(JwtAuthGuard)
-    @Post('add-discussion')
-    addPrivateMessage(@Req() req, @Body() discu: DiscussionFront) {
+	@Post('add-discussion')
+	addPrivateMessage(@Req() req, @Body() body) {
 		const user: User = req.user;
-		console.log('add Discussion receive', discu);
-        throw new NotImplementedException('This feature is under developpement.');
-    }
+		const discu: DiscussionFront = body;
+		console.log('add Discussion receive', discu, 'body', body);
+		throw new NotImplementedException('This feature is under developpement.');
+	}
 
 	@UseGuards(JwtAuthGuard)
-    @Post('add-channel')
-    addChannel(@Req() req, @Body() channel: ChannelFront) {
+	@Post('add-channel')
+	addChannel(@Req() req, @Body() body) {
 		const user: User = req.user;
-		console.log('add Channel receive', channel);
-        throw new NotImplementedException('This feature is under developpement.');
-    }
+		const channel: ChannelFront = body;
+		console.log('add Channel receive', channel, 'body', body);
+		throw new NotImplementedException('This feature is under developpement.');
+	}
 
 	@UseGuards(JwtAuthGuard)
-    @Post('leave-channel')
-    leaveChannel(@Req() req, @Body() body: any) {
-		const user: User = req.user;
-		const channelId: number = body.id;
-		console.log('leave channel id n°', channelId);
-        throw new NotImplementedException('This feature is under developpement.');
-    }
-
-	@UseGuards(JwtAuthGuard)
-    @Post('delete-channel')
-    deleteChannel(@Req() req, @Body() body: any) {
+	@Post('leave-channel')
+	leaveChannel(@Req() req, @Body() body: any) {
 		const user: User = req.user;
 		const channelId: number = body.id;
-		console.log('delete channel id n°', channelId);
-        throw new NotImplementedException('This feature is under developpement.');
-    }
+		console.log('leave channel id n°', channelId, 'body', body);
+		throw new NotImplementedException('This feature is under developpement.');
+	}
 
 	@UseGuards(JwtAuthGuard)
-    @Post('delete-discussion')
-    deletePrivateMessage(@Req() req, @Body() body: any) {
+	@Post('delete-channel')
+	deleteChannel(@Req() req, @Body() body: any) {
 		const user: User = req.user;
 		const channelId: number = body.id;
-		console.log('delete private message id n°', channelId, '(id of user ou discu ? idk)');
-        throw new NotImplementedException('This feature is under developpement.');
-    }
+		console.log('delete channel id n°', channelId, 'body', body);
+		throw new NotImplementedException('This feature is under developpement.');
+	}
 
 	@UseGuards(JwtAuthGuard)
-    @Post('add-admin')
-    addAdmin(@Req() req, @Body() body: any) {
+	@Post('delete-discussion')
+	deletePrivateMessage(@Req() req, @Body() body: any) {
 		const user: User = req.user;
-		const users_ids: number[] = body.users;
-		console.log('add admin', users_ids);
-        throw new NotImplementedException('This feature is under developpement.');
-    }
+		const channelId: number = body.id;
+		console.log('delete private message id n°', channelId, '(id of user ou discu ? idk)', 'body', body);
+		throw new NotImplementedException('This feature is under developpement.');
+	}
 
 	@UseGuards(JwtAuthGuard)
-    @Post('ban-users')
-    banUsers(@Req() req, @Body() body: any) {
+	@Post('add-admin')
+	addAdmin(@Req() req, @Body() body: any) {
 		const user: User = req.user;
 		const users_ids: number[] = body.users;
-		console.log('ban users', users_ids);
-        throw new NotImplementedException('This feature is under developpement.');
-    }
+		console.log('add admin', users_ids, 'body', body);
+		throw new NotImplementedException('This feature is under developpement.');
+	}
 
 	@UseGuards(JwtAuthGuard)
-    @Post('mute-users')
-    muteUsers(@Req() req, @Body() body: any) {
+	@Post('ban-users')
+	banUsers(@Req() req, @Body() body: any) {
 		const user: User = req.user;
 		const users_ids: number[] = body.users;
-		console.log('mute users', users_ids);
-        throw new NotImplementedException('This feature is under developpement.');
-    }
+		console.log('ban users', users_ids, 'body', body);
+		throw new NotImplementedException('This feature is under developpement.');
+	}
 
 	@UseGuards(JwtAuthGuard)
-    @Post('kick-users')
-    kickMembers(@Req() req, @Body() body: any) {
+	@Post('mute-users')
+	muteUsers(@Req() req, @Body() body: any) {
 		const user: User = req.user;
 		const users_ids: number[] = body.users;
-		console.log('kick users', users_ids);
-        throw new NotImplementedException('This feature is under developpement.');
-    }
+		console.log('mute users', users_ids, 'body', body);
+		throw new NotImplementedException('This feature is under developpement.');
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Post('kick-users')
+	kickMembers(@Req() req, @Body() body: any) {
+		const user: User = req.user;
+		const users_ids: number[] = body.users;
+		console.log('kick users', users_ids, 'body', body);
+		throw new NotImplementedException('This feature is under developpement.');
+	}
 }
