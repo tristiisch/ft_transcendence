ab02d7f91457c2c3a19b7b17018d43b47cd2899e
Tristiisch <tristiisch@outlook.fr>
2022-09-16 01:33:35 +0200

    up: try fix macos docker version

--- autodocker.sh
+++ autodocker.sh
@@ -1,22 +1,61 @@
-#/bin/bash -e
+#!/bin/bash -e
 
-while docker-compose logs postgreSQL | grep -q "ready in";
+# 10 mins
+MAX_SECONDES=600
+TIME_CICLE=10
+
+i=0
+
+function timeoutCheck() {
+    if [[ "$i" -ge "$MAX_SECONDES" ]]; then
+        echo "\!/ Timeout \!/ after $i secondes"
+        exit 1
+    fi
+}
+
+while ! docker container inspect front api postgreSQL pgAdmin &> /dev/null;
 do
-    echo "Waiting for Front to end"
-    sleep 10
+    echo "Waiting for containers to be create ... $i secondes"
+    sleep $TIME_CICLE
+	let "i = i + $TIME_CICLE"
+    timeoutCheck
+done
+
+while ! docker-compose logs front | grep -q "ready in";
+do
+    echo "Waiting for Front to end ... $i secondes"
+    sleep $TIME_CICLE
+	let "i = i + $TIME_CICLE"
+    timeoutCheck
 done
 echo "Front finish !"
 
-while docker-compose logs postgreSQL | grep -q "database system is ready to accept connections";
+while ! docker-compose logs postgreSQL | grep -q "database system is ready to accept connections";
 do
-    echo "Waiting for postgreSQL to end"
-    sleep 10
+    echo "Waiting for postgreSQL to end ... $i secondes"
+    sleep $TIME_CICLE
+	let "i = i + $TIME_CICLE"
+    timeoutCheck
 done
 echo "postgreSQL finish !"
 
-while docker-compose logs api | grep -q "Nest application successfully started";
+while ! docker-compose logs api | grep -q "Nest application successfully started";
 do
-    echo "Waiting for API to end"
-    sleep 10
+    echo "Waiting for API to end ... $i secondes"
+    sleep $TIME_CICLE
+	let "i = i + $TIME_CICLE"
+    timeoutCheck
 done
 echo "API finish !"
+
+while ! docker-compose logs pgAdmin | grep -q "Listening at";
+do
+    echo "Waiting for pgAdmin to end ... $i secondes"
+    sleep $TIME_CICLE
+	let "i = i + $TIME_CICLE"
+    timeoutCheck
+done
+echo "pgAdmin finish !"
+
+echo "Now close all containers"
+docker-compose stop
