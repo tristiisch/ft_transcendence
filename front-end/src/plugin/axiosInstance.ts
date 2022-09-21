0ee0e9ca7e48d2214eaf687be6ea78cdac08a422
jlaronch <jlaronch@student.42lyon.fr>
2022-09-21 16:05:27 +0200

    Update axios token

--- front-end/src/plugin/axiosInstance.ts
+++ front-end/src/plugin/axiosInstance.ts
@@ -1,15 +1,27 @@
 import axios from 'axios';
 import { useUserStore } from '@/stores/userStore';
 import { useToast } from 'vue-toastification';
+import AuthService from '@/services/AuthService';
 
 const instance = axios.create({
-	baseURL: 'http://localhost:3000/api',
+	//baseURL: 'http://localhost:3000/api',
 	//headers: {'Content-Type': 'application/json'}
-	// baseURL: import.meta.env.VITE_API_URL // It send all env vars to client \!/
+	baseURL: import.meta.env.VITE_API_URL // It send all env vars to client \!/
 });
 
-instance.interceptors.response.use(
-	function (response) {
+instance.interceptors.request.use(function (config) {
+	const userStore = useUserStore();
+	console.log(userStore.isLoggedIn)
+	if (!userStore.isLoggedIn) config.headers!.Authorization = `Bearer ${userStore.userAuth.token_jwt}`
+	else config.headers!.Authorization = `Bearer ${AuthService.getJwtToken()}`
+	//config.headers!.Authorization =  token ? `Bearer ${token}` : '';
+		return config;
+	}, function (error) {
+		return Promise.reject(error);
+	}
+);
+  
+instance.interceptors.response.use(function (response) {
 		return response;
 	},
 	function (error) {
