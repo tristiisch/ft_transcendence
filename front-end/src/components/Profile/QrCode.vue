de767786a5555985da9282c119490a05e9fd98af
jlaronch <jlaronch@student.42lyon.fr>
2022-09-29 12:32:31 +0200

    Change input 2fa to text

--- front-end/src/components/Profile/QrCode.vue
+++ front-end/src/components/Profile/QrCode.vue
@@ -74,7 +74,7 @@ function validate2FA() {
 				<p class="text-red-100 text-xs">Please scan the QRCode in Google's Authenticator and send the 2FA code</p>
 			</div>
 			<div class="flex justify-center w-full gap-4">
-				<input type="password" name="twoFaCode" v-model="twoFaCode" placeholder="2FA code" class="text-center text-xs text-blue-600 placeholder:text-slate-300 bg-neutral-100 border border-blue-600 rounded-lg placeholder:text-center" />
+				<input type="text" name="twoFaCode" v-model="twoFaCode" placeholder="2FA code" class="text-center text-xs text-blue-600 placeholder:text-slate-300 bg-neutral-100 border border-blue-600 rounded-lg placeholder:text-center" />
 				<base-button @click="validate2FA" class="text-sm w-20 py-1 sm:py-2 rounded-lg bg-neutral-100 text-blue-600 border border-blue-600 hover:bg-blue-600 hover:text-neutral-100">Send</base-button>
 			</div>
 		</form>
